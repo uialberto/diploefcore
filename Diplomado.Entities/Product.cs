f941d61812737382392376e6dda78eb50eea46ce
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-03-19 22:47:27 -0400

    Agregar archivos de proyecto.

---
+++ Diplomado.Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Diplomado.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
