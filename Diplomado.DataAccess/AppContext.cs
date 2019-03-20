df8622c515ba901530467b33b7b912e8a6b971a5
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-03-19 23:19:22 -0400

    introducción a las migraciones

---
+++ Diplomado.DataAccess/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Diplomado.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Diplomado.DataAccess
+{
+    public class AppContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=Northwind;Trusted_Connection=True;"
+            );
+        }
+    }
+}
