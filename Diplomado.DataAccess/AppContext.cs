8af8897317344569e569c9041e9936b3af298681
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-04-29 22:00:16 -0400

    Desarrollo de aplicaciones .NET Framework con EF Core - ASP.NET MVC

--- Diplomado.DataAccess/AppContext.cs
+++ Diplomado.DataAccess/AppContext.cs
@@ -14,9 +14,8 @@ namespace Diplomado.DataAccess
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //Server=localhost;Database=aspnet-MVCAuthSQL;Trusted_Connection=False;MultipleActiveResultSets=true;User Id=SA;Password=my-secret-password;
-            optionsBuilder.UseSqlServer(
-                "Server=.\\odin;Database=Northwind_Migra;Trusted_Connection=False;User Id=desa;Password=desa"
-            );
+
+            optionsBuilder.UseSqlServer("Server=.\\odin;Database=Northwind_Migra;Trusted_Connection=False;User Id=desa;Password=desa");
 
             //optionsBuilder.UseSqlServer(
             //    "Server=(localdb)\\mssqllocaldb;Database=Northwind;Trusted_Connection=True;"
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Diplomado.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppContext = Diplomado.DataAccess.AppContext;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var categories = new List<Category>();
+            using (var context = new AppContext())
+            {
+                categories = context.Categories.ToList();
+            }
+            return View(categories);
+        }
+        [HttpPost]
+        public ActionResult Index(string categoryName)
+        {            
+            using (var context = new AppContext())
+            {
+                var entity = new Category()
+                {
+                    CategoryName = categoryName
+                };
+                context.Categories.Add(entity);
+                var result = context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
