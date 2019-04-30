0e153288f7129b2c6b7c0d6db65fff3407712e98
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-04-29 22:59:34 -0400

    Desarrollo de aplicaciones .NET Framework con EF Core - ASP.NET MVC Operaciones CRUD

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -34,5 +34,36 @@ namespace WebApp.Controllers
             }
             return RedirectToAction("Index");
         }
+        
+        public ActionResult Details(int id)
+        {
+            var category = new Category();
+            using (var context = new AppContext())
+            {
+                category = context.Categories.Find(id); 
+                
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Persist(Category category, string btnUpdate, string btnDelete)
+        {            
+            using (var context = new AppContext())
+            {
+                if (btnUpdate != null)
+                {
+                    context.Categories.Update(category);
+                }else if (btnDelete != null)
+                {
+                    context.Categories.Remove(category);
+                }
+
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
