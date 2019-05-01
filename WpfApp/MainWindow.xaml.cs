ae32a9dff131b6bfa2d990a40db02f6de86b70d4
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-05-01 15:19:55 -0400

    Desarrollo de aplicaciones .NET Framework con EF Core - WPF

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using Diplomado.Entities;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp
 {
@@ -20,9 +9,30 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Diplomado.DataAccess.AppContext _context = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            _context = new Diplomado.DataAccess.AppContext();
+            LoadCategories();
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            var entity = new Category()
+            {
+                CategoryName = txtCategoryName.Text
+            };
+            _context.Categories.Add(entity);
+            var records = _context.SaveChanges();
+            LoadCategories();
+        }
+
+        void LoadCategories()
+        {
+            var result = _context.Categories.ToList();
+            DGCategories.ItemsSource = result;
         }
     }
 }
