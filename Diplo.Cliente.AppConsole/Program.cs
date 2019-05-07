cfae0ad0af77f3fd6a8c5f29dcc599f3359fddb3
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-05-07 00:23:10 -0400

    Desarrollo de aplicaciones .NET Framework con EF Core - Database-First, Scaffolding.

--- Diplo.Cliente.AppConsole/Program.cs
+++ Diplo.Cliente.AppConsole/Program.cs
@@ -8,7 +8,26 @@ namespace Diplo.Cliente.AppConsole
         static void Main(string[] args)
         {
 
-           
+            using (var context = new NorthwindContext())
+            {
+                Console.WriteLine("Ingres nombre de categoria:");
+                var categoryName = Console.ReadLine();
+
+                var newCategory = new Categories()
+                {
+                    CategoryName = categoryName
+                };
+                context.Categories.Add(newCategory);
+                var result = context.SaveChanges();
+                Console.WriteLine($"{result} registro guardado en la BD.");
+                Console.WriteLine("Categorias:");
+                foreach (var item in context.Categories)
+                {
+
+                    Console.WriteLine($"{item.CategoryId} {item.CategoryName}");
+                }
+
+            }
         }
     }
 }
