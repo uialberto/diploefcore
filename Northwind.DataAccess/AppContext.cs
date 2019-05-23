d0aa476cfc1cc3291ccd10006089947240a6fa2c
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-05-22 22:37:32 -0400

    Desarrollo de aplicaciones .NET Core con EF Core

---
+++ Northwind.DataAccess/AppContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Entities;
+
+namespace Northwind.DataAccess
+{
+    public class AppContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Server=localhost;Database=aspnet-MVCAuthSQL;Trusted_Connection=False;MultipleActiveResultSets=true;User Id=SA;Password=my-secret-password;
+
+            optionsBuilder.UseSqlServer("Server=.\\odin;Database=CoreNorthwind;Trusted_Connection=False;User Id=desa;Password=desa");
+
+            //optionsBuilder.UseSqlServer(
+            //    "Server=(localdb)\\mssqllocaldb;Database=Northwind;Trusted_Connection=True;"
+            //);
+        }
+    }
+}
