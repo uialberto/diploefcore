47d2c57c37736c7f58d6ec26fca8ac0a71c2e911
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-06-05 22:38:02 -0400

    Desarrollo de aplicaciones Xamarin con EF Core

--- Cloud.Northwind.DataAccess/AppContext.cs
+++ Cloud.Northwind.DataAccess/AppContext.cs
@@ -11,13 +11,21 @@ namespace Cloud.Northwind.DataAccess
         public virtual DbSet<Product> Products { get; set; }
         public virtual DbSet<Category> Categories { get; set; }
 
+        private string _dbPath = ""; /*CloudNorthWindLite.db*/
+
+        public AppContext() : this("CloudNorthWindLite.db") { }
+        public AppContext(string dbPath)
+        {
+            this._dbPath = dbPath;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //Server=localhost;Database=aspnet-MVCAuthSQL;Trusted_Connection=False;MultipleActiveResultSets=true;User Id=SA;Password=my-secret-password;
 
             //optionsBuilder.UseSqlServer("Server=.\\odin;Database=CoreNorthwind;Trusted_Connection=False;User Id=desa;Password=desa");
 
-            optionsBuilder.UseSqlite("Data Source=CloudNorthWindLite.db");
+            optionsBuilder.UseSqlite($"Data Source={_dbPath}");
 
             //optionsBuilder.UseSqlServer(
             //    "Server=(localdb)\\mssqllocaldb;Database=Northwind;Trusted_Connection=True;"
--- Cloud.Northwind.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Cloud.Northwind.DataAccess
-{
-    public class Class1
-    {
-    }
-}
