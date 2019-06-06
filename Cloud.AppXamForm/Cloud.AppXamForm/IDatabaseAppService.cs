075cb94359788f90f8d9b6de5424111bbfd69e06
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-06-05 23:25:02 -0400

    Desarrollo de aplicaciones Xamarin con EF - Migraciones, DependencyService.

---
+++ Cloud.AppXamForm/Cloud.AppXamForm/IDatabaseAppService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloud.AppXamForm
+{
+    public interface IDataBaseAppService
+    {
+        string GetFullPath(string dbFileName);
+    }
+}
