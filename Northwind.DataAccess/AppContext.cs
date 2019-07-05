b88a086684849ed60ae8b406a10a471dbfbead41
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-07-05 00:13:03 -0400

    Trabajando con cadenas de conexión - ASP.NET Core

--- Northwind.DataAccess/AppContext.cs
+++ Northwind.DataAccess/AppContext.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Northwind.Entities;
 
 namespace Northwind.DataAccess
@@ -17,7 +19,11 @@ namespace Northwind.DataAccess
 
             //optionsBuilder.UseSqlServer("Server=.\\odin;Database=CoreNorthwind;Trusted_Connection=False;User Id=desa;Password=desa");
 
-            optionsBuilder.UseSqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["AppContext"].ConnectionString);
+            //optionsBuilder.UseSqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["AppContext"].ConnectionString); //SystemConfiguration
+
+            var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false).Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("AppContext")); //SystemConfiguration
 
             //optionsBuilder.UseSqlServer(
             //    "Server=(localdb)\\mssqllocaldb;Database=Northwind;Trusted_Connection=True;"
