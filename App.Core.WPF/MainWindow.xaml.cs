ebfec5d92fd9a9aa4d75ca3fdb9c9c913e326694
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-07-25 22:45:58 -0400

    actualizacion app core WPF ajust

---
+++ App.Core.WPF/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Core.Northwind.DataAccess;
+using Core.Northwind.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppCoreWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AppCoreContext _context = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _context = new AppCoreContext();
+            LoadCategories();
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            var entity = new Category()
+            {
+                CategoryName = txtCategoryName.Text
+            };
+            _context.Categories.Add(entity);
+            var records = _context.SaveChanges();
+            LoadCategories();
+        }
+
+        void LoadCategories()
+        {
+            var result = _context.Categories.ToList();
+            DGCategories.ItemsSource = result;
+        }
+
+        private void DGCategories_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Category category = DGCategories.SelectedItem as Category;
+            var ventana = new UpdateAndDelete(_context, category);
+            ventana.ShowDialog();
+        }
+
+        private void BtnCargar_OnClick(object sender, RoutedEventArgs e)
+        {
+            LoadCategories();
+        }
+    }
+}
---
+++ App.Core.WPF/UpdateAndDelete.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Core.Northwind.DataAccess;
+using Core.Northwind.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppCoreWPF
+{
+    /// <summary>
+    /// Lógica de interacción para UpdateAndDelete.xaml
+    /// </summary>
+    public partial class UpdateAndDelete : Window
+    {
+        private AppCoreContext _context = null;
+        private Category _category = null;
+        public UpdateAndDelete(AppCoreContext context, Category category)
+        {
+            InitializeComponent();
+            _context = context;
+            _category = category;
+
+            txtCategoryId.Text = category?.CategoryID.ToString();
+            txtCategoryName.Text = category?.CategoryName;
+        }
+
+        private void BtnUpdate_OnClick(object sender, RoutedEventArgs e)
+        {
+            _category.CategoryName = txtCategoryName.Text;
+            _context.Categories.Update(_category);
+            var result = _context.SaveChanges();
+            if (result > 0)
+            {
+                MessageBox.Show("Datos Actualizados");
+            }
+        }
+
+        private void BtnEliminar_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            _context.Categories.Remove(_category);
+            var result = _context.SaveChanges();
+            if (result > 0)
+            {
+                MessageBox.Show("Datos Eliminados");
+            }
+        }
+    }
+}
