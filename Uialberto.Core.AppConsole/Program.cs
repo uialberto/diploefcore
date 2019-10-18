7c8695b78617b049dc3661ea8dff93ba71a9bd01
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-10-17 21:49:15 -0400

    creacion de migraciones automaticas

--- Uialberto.Core.AppConsole/Program.cs
+++ Uialberto.Core.AppConsole/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using Uialberto.Northwind.Entities;
 
 namespace Uialberto.Core.AppConsole
@@ -9,22 +13,21 @@ namespace Uialberto.Core.AppConsole
         {
             using (var context = new Northwind.DataAccess.AppContext())
             {
-                Console.WriteLine("Introduce el nombre de gategoras");
-                var category = Console.ReadLine();
-                var newCategory = new Category
-                {
-                    CategoryName = category
-                };
-                context.Categories.Add(newCategory);
-                var rowAffected = context.SaveChanges();
-                Console.WriteLine($"Número de registros afectados {rowAffected}");
-                Console.WriteLine("Categorias:");
-                foreach (var entity in context.Categories)
-                {
-                    Console.WriteLine($"{entity.CategoryID}, {entity.CategoryName}");
-                }
 
-                Console.ReadLine();
+                ////context.Database.Migrate(); // CASO 1
+
+
+                // CASO 2
+                ////Console.WriteLine("Aplicando Migraciones Database");
+                //var migrator = context.Database.GetInfrastructure()
+                //    .GetService<IMigrator>();
+
+                //migrator.Migrate();
+                //Console.ReadLine();
+
+                // CASO 3, No Usar si se va trabajar con migraciones
+                context.Database.EnsureCreated();   
+
 
             }
         }
