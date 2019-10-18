fab962ec70a7532e4e6c0490860b001bd9da818d
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-10-17 22:21:23 -0400

    update product rename name

---
+++ Uialberto.Northwind.DataAccess/Migrations/20191018022108_UpdateProductRename.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Uialberto.Northwind.DataAccess.Migrations
+{
+    public partial class UpdateProductRename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "Products",
+                newName: "NombreProducto");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NombreProducto",
+                table: "Products",
+                newName: "ProductName");
+        }
+    }
+}
--- Uialberto.Northwind.Entities/Product.cs
+++ Uialberto.Northwind.Entities/Product.cs
@@ -7,7 +7,7 @@ namespace Uialberto.Northwind.Entities
     public class Product
     {
         public int ProductID { get; set; }
-        public string ProductName { get; set; }
+        public string NombreProducto { get; set; }
         public decimal UnitPrice { get; set; }
         public int UnitsInStock { get; set; }
         public int CategoryID { get; set; }
