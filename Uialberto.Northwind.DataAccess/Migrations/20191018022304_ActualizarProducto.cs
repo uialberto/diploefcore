ce512ad02093faabb06b46ad662953eecfeae417
uibasoft <uibasoft@outlook.com>
2019-10-17 22:23:44 -0400

    Actualizacion producto por producto

---
+++ Uialberto.Northwind.DataAccess/Migrations/20191018022304_ActualizarProducto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Uialberto.Northwind.DataAccess.Migrations
+{
+    public partial class ActualizarProducto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "Products",
+                newName: "Producto");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Producto",
+                table: "Products",
+                newName: "ProductName");
+        }
+    }
+}
