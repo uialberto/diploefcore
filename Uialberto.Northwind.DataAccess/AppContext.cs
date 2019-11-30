58bd640945323cff6807e5295c74f820819f37e4
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-11-30 12:45:53 -0400

    implementacion create database

--- Uialberto.Northwind.DataAccess/AppContext.cs
+++ Uialberto.Northwind.DataAccess/AppContext.cs
@@ -6,24 +6,35 @@ namespace Uialberto.Northwind.DataAccess
 {
     public class AppContext : DbContext
     {
+        public AppContext(DbContextOptions options) : base(options)
+        {
+
+        }
         public virtual DbSet<Product> Products { get; set; }
         public virtual DbSet<Category> Categories { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //Server=localhost;Database=aspnet-MVCAuthSQL;Trusted_Connection=False;MultipleActiveResultSets=true;User Id=SA;Password=my-secret-password;
-
-            optionsBuilder.UseSqlServer("Server=.\\odin;Database=CoreNorthwind;Trusted_Connection=False;User Id=desa;Password=desa");
-
-            //optionsBuilder.UseSqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["AppContext"].ConnectionString); //SystemConfiguration
-
-            //var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false).Build();
-
-            //optionsBuilder.UseSqlServer(config.GetConnectionString("AppContext")); //SystemConfiguration
-
-            //optionsBuilder.UseSqlServer(
-            //    "Server=(localdb)\\mssqllocaldb;Database=Northwind;Trusted_Connection=True;"
-            //);
+            //base.OnModelCreating(modelBuilder); 
+            modelBuilder.Entity<Product>()
+                .Property(p => p.UnitPrice)
+                .HasColumnType("decimal(8,2)");
         }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    //Server=localhost;Database=aspnet-MVCAuthSQL;Trusted_Connection=False;MultipleActiveResultSets=true;User Id=SA;Password=my-secret-password;
+
+        //    optionsBuilder.UseSqlServer("Server=.\\odin;Database=NorthwindDbCreate;Trusted_Connection=False;User Id=desa;Password=desa");
+
+        //    //optionsBuilder.UseSqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["AppContext"].ConnectionString); //SystemConfiguration
+
+        //    //var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false).Build();
+
+        //    //optionsBuilder.UseSqlServer(config.GetConnectionString("AppContext")); //SystemConfiguration
+
+        //    //optionsBuilder.UseSqlServer(
+        //    //    "Server=(localdb)\\mssqllocaldb;Database=Northwind;Trusted_Connection=True;"
+        //    //);
+        //}
     }
 }
