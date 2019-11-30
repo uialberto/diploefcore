3030d09ef9224ce12cef5638cf1d1e6dcf835812
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2019-11-30 13:02:27 -0400

    implementacion create db base

--- Uialberto.Core.AppConsole/Program.cs
+++ Uialberto.Core.AppConsole/Program.cs
@@ -7,26 +7,26 @@ namespace Uialberto.Core.AppConsole
     {
         static void Main(string[] args)
         {
-            using (var context = new Northwind.DataAccess.AppContext())
-            {
-                Console.WriteLine("Introduce el nombre de gategoras");
-                var category = Console.ReadLine();
-                var newCategory = new Category
-                {
-                    CategoryName = category
-                };
-                context.Categories.Add(newCategory);
-                var rowAffected = context.SaveChanges();
-                Console.WriteLine($"Número de registros afectados {rowAffected}");
-                Console.WriteLine("Categorias:");
-                foreach (var entity in context.Categories)
-                {
-                    Console.WriteLine($"{entity.CategoryID}, {entity.CategoryName}");
-                }
+            //using (var context = new Northwind.DataAccess.AppContext())
+            //{
+            //    Console.WriteLine("Introduce el nombre de gategoras");
+            //    var category = Console.ReadLine();
+            //    var newCategory = new Category
+            //    {
+            //        CategoryName = category
+            //    };
+            //    context.Categories.Add(newCategory);
+            //    var rowAffected = context.SaveChanges();
+            //    Console.WriteLine($"Número de registros afectados {rowAffected}");
+            //    Console.WriteLine("Categorias:");
+            //    foreach (var entity in context.Categories)
+            //    {
+            //        Console.WriteLine($"{entity.CategoryID}, {entity.CategoryName}");
+            //    }
 
-                Console.ReadLine();
+            //    Console.ReadLine();
 
-            }
+            //}
         }
     }
 }
--- WebAppCore/Startup.cs
+++ WebAppCore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +34,11 @@ namespace WebAppCore
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<Uialberto.Northwind.DataAccess.AppContext>(option => 
+            option.UseSqlServer("Server=.\\odin;Database=NorthwindDbCreate;Trusted_Connection=False;User Id=desa;Password=desa"
+                , x => x.CommandTimeout(60)));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
